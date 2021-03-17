93a73674aa59c49fb6460a1437a0699d4c316a0d
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-03-17 09:04:49 +0900

    アイキャッチの説明文を調整

--- dental-site/src/pages/index.tsx
+++ dental-site/src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
       <main>
         <div className={styles.eyecache}>
-          <Image src="/img_main.jpg" alt="医院内の写真" width={1401} height={608} layout={"responsive"} />
+          <img src="/img_main.jpg" alt="医院内の写真" />
           <div className={styles.officeInfo}>
             <div>
               <p className={styles.tel}><i className="fas fa-phone fa-rotate-90"></i> 052-0123-4567</p>
@@ -20,7 +20,7 @@ export default function Home() {
             </div>
             <a href="" className={styles.form}><i className="fas fa-envelope"></i>　お問い合わせ</a>
           </div>
-          <p>
+          <p className={styles.explaine}>
             治療期間も快適にお過ごしいただきたいから<br />
             患者様ひとりひとりに合った<br />
             最適な治療法をご提案
@@ -29,14 +29,7 @@ export default function Home() {
       </main >
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+
       </footer>
     </div >
   )
