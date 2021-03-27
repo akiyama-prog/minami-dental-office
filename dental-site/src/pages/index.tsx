63921e6792f5173e6de40b640212d270e313e848
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-03-27 19:51:11 +0900

    headの設定

--- dental-site/src/pages/index.tsx
+++ dental-site/src/pages/index.tsx
@@ -8,6 +8,8 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>みなみ矯正歯科</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
         <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet"></link>
         <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet"></link>
