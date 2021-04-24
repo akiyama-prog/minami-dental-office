5b4769b5b98b04a54e4714a4c7022ea5f1338ef6
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-04-24 09:50:57 +0900

    サンクスページレスポンシブ化

--- dental-site/src/pages/thunx.tsx
+++ dental-site/src/pages/thunx.tsx
@@ -2,8 +2,11 @@ import styles from '../styles/Form.module.scss'
 import Head from '../../components/Head'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import { useMediaQuery } from 'react-responsive'
 
 export default function Form() {
+    const isPC: boolean = useMediaQuery({ query: '(min-width: 769px)' })
+    const isMobile: boolean = useMediaQuery({ query: '(max-width: 768px)' })
     return (
         <div className={styles.container}>
             <Head title={'みなみ矯正歯科'}></Head>
@@ -12,9 +15,16 @@ export default function Form() {
                 <div className={styles.thunx}>
                     <h2>送信完了</h2>
                     <hr className={styles.hr}></hr>
-                    <p>この度はお問い合わせ及びご予約いただき誠にありがとうございました。<br />
-                        改めて担当者よりご連絡をさせていただきます。
-                    </p>
+                    {isPC && (
+                        <p>この度はお問い合わせ及びご予約いただき誠にありがとうございました。<br />
+                            改めて担当者よりご連絡をさせていただきます。
+                        </p>
+                    )}
+                    {isMobile && (
+                        <p>この度はお問い合わせ及びご予約いただき<br />誠にありがとうございました。<br />
+                            改めて担当者よりご連絡をさせていただきます。
+                        </p>
+                    )}
                 </div>
             </main>
             <Footer></Footer>
