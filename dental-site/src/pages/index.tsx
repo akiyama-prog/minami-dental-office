2a514f6a0fee32cab6286a2e5b14a177d7bbe35e
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-05-10 22:15:07 +0900

    吹き出し部分のレスポンシブ修正

--- dental-site/src/pages/index.tsx
+++ dental-site/src/pages/index.tsx
@@ -156,9 +156,6 @@ export default function Home() {
             )}
           </div>
           <div className={styles.forBeginner}>
-            {isMobile && (
-              <img src="/bg_fukidashi_sp.png" alt="吹き出し" className={styles.bgFukidashi} />
-            )}
             <div className={styles.innerContent}>
               <div className={styles.contents}>
                 <p className={styles.title}><img src="ico_beginner.png" alt="初心者マーク" /> 初めての方へ<br />
