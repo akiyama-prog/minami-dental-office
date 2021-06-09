8761f8b8b4847a44679ba517dd988a83e8f7fc1b
akiyama-prog <53988066+akiyama-prog@users.noreply.github.com>
2021-06-09 23:34:48 +0900

    デプロイ時エラー修正

--- dental-site/components/Head.tsx
+++ dental-site/components/Head.tsx
@@ -9,7 +9,7 @@ export default function head(props) {
             <link rel="icon" href="/favicon.ico" />
             <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet"></link>
             <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet"></link>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"></link>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"></link>
         </Head>
     );
 }
